1b5e8611fa96db259eddcb59074e48b3d8420f1f
Oscar Roberts <roberts_oscar@hotmail.com>
2024-09-06 14:50:55 -0300

    se refactoriza

--- area-de-circulo.py
+++ area-de-circulo.py
@@ -1,13 +1,10 @@
 import math
 
-# 1. solicitar que se ingrese el radio del círculo
-radio = input("por favor ingrese el radio del círculo: ")
+# 1. solicitar que se ingrese el radio del círculo y castear a flotante ya que el input devuelve un str
+radio = float(input("por favor ingrese el radio del círculo: "))
 
 # Reemplazar la coma por un punto si el usuario la ingresó
 radio = radio.replace(',', '.')
-
-# Conviertir el radio a un número decimal (float)
-radio = float(radio)
 
 # 2. calcular el área del círculo, utilizando la fórmula area = pi * radio^2 
 area = math.pi * (radio ** 2)
